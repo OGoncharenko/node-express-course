fdae0541aa28787400db9a27770195127bcae65d
OGoncharenko <olyas.goncharenko@gmail.com>
2024-06-28 01:09:19 -0700

    Answers for lesson 1

---
+++ 01-node-tutorial/answers/prompter.js
@@ -0,0 +1,71 @@
+const http = require("http");
+var StringDecoder = require("string_decoder").StringDecoder;
+
+const getBody = (req, callback) => {
+  const decode = new StringDecoder("utf-8");
+  let body = "";
+  req.on("data", function (data) {
+    body += decode.write(data);
+  });
+  req.on("end", function () {
+    body += decode.end();
+    const body1 = decodeURI(body);
+    const bodyArray = body1.split("&");
+    const resultHash = {};
+    bodyArray.forEach((part) => {
+      const partArray = part.split("=");
+      resultHash[partArray[0]] = partArray[1];
+    });
+    callback(resultHash);
+  });
+};
+
+// here, you could declare one or more variables to store what comes back from the form.
+let item = "Share your mood today: happy, tired, grateful";
+
+// here, you can change the form below to modify the input fields and what is displayed.
+// This is just ordinary html with string interpolation.
+const form = () => {
+  return `
+  <body>
+  <p>${item}</p>
+  <form method="POST">
+  <input name="item"></input>
+  <button type="submit">Submit</button>
+  </form>
+  </body>
+  `;
+};
+
+const server = http.createServer((req, res) => {
+  console.log("req.method is ", req.method);
+  console.log("req.url is ", req.url);
+  if (req.method === "POST") {
+    getBody(req, (body) => {
+      console.log("The body of the post is ", body);
+      if (body["item"]) {
+        const userMood = body["item"].toLowerCase();
+        if (userMood === "happy") {
+          item = 'Congratulations!';
+        } else if (userMood === "tired") {
+          item = 'Sorry! Go take a nap!';
+        } else if (userMood === "grateful") {
+          item = 'That is so great!';
+        } else {
+          item = 'Please enter either happy, tired, or grateful.'
+        }
+      } else {
+        item = "Nothing was entered.";
+      }
+      res.writeHead(303, {
+        Location: "/",
+      });
+      res.end();
+    });
+  } else {
+    res.end(form());
+  }
+});
+
+server.listen(3001);
+console.log("The server is listening on port 3000.");
